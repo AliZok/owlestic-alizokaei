671b13d8199e102c3df6f1673f726ea6347e13d4
ali <ali.zokaei.1367@gmail.com>
2025-03-13 16:07:22 +0330

    install material ui

--- src/app/my-test/page.tsx
+++ src/app/my-test/page.tsx
@@ -1,7 +1,18 @@
-export default function myTest(){
-    return(
-        <div>
-            this is a test
-        </div>
+'use client';
+import React from 'react';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+
+export default function myTest() {
+    return (
+        <Container>
+            <h1>Welcome to Next.js with Material-UI!</h1>
+            <Button variant="contained" color="primary">
+                Click Me
+            </Button>
+            <Button variant="text">Text</Button>
+            <Button variant="contained">Contained</Button>
+            <Button variant="outlined">Outlined</Button>
+        </Container>
     )
 }
