05f8a28c1126bbbfc2c3d36823bd83fc4e44969c
ali <ali.zokaei.1367@gmail.com>
2025-03-13 19:44:43 +0330

    hi hello

--- src/components/orders-table.tsx
+++ src/components/orders-table.tsx
@@ -44,6 +44,7 @@ function createData(
 }
 
 const rows = [
+  { name: 'alizoka', calories: 'bagher', fat: 'gahsem', carbs: 'system', protein: 'jasem' },
   createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
   createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
   createData('Eclair', 262, 16.0, 24, 6.0),
@@ -152,12 +153,12 @@ export function OrdersTable({
           </Select>
         </div>
       </div>
-      <div className="rounded-md border">
+      <div className="rounded-md ">
         <TableContainer component={Paper}>
           <Table sx={{ minWidth: 650 }} aria-label="simple table">
             <TableHead>
               <TableRow>
-                <TableCell>Dessert (100g serving)</TableCell>
+                <TableCell>Name</TableCell>
                 <TableCell align="right">Calories</TableCell>
                 <TableCell align="right">Fat&nbsp;(g)</TableCell>
                 <TableCell align="right">Carbs&nbsp;(g)</TableCell>
