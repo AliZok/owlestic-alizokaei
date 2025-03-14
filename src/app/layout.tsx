3eeb399435d7e8486d405021e00b2ae76defa18d
ali <ali.zokaei.1367@gmail.com>
2025-03-14 13:30:04 +0330

    save

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,17 +2,6 @@ import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
-import createEmotionCache from '@/utils/createEmotionCache';
-
-import { CacheProvider } from '@emotion/react';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-
-const clientSideEmotionCache = createEmotionCache();
-
-const theme = createTheme({
-  direction: 'rtl',
-});
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -32,4 +21,3 @@ export default function RootLayout({
     </html>
   )
 }
-
