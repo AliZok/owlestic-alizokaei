ec9827b2cdacd91a0d30fc9c68617ad65c0ad6b0
ali <ali.zokaei.1367@gmail.com>
2025-03-14 13:38:49 +0330

    scss

--- src/components/dashboard-shell.tsx
+++ src/components/dashboard-shell.tsx
@@ -8,7 +8,7 @@ export function DashboardShell({ children, className, ...props }: DashboardShell
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr]" dir="rtl">
       <DashboardNav />
-      <main className="flex w-full flex-col overflow-hidden">
+      <main className="flex w-full flex-col overflow-hidden ">
         <div className="flex-1 space-y-4 p-8 pt-6">
           <div className={cn("flex flex-col space-y-6", className)} {...props}>
             {children}
